8de505a458933124626183881654d4f05172d0f2
bijaymandal01 <bijaymandal01py@gmail.com>
2026-05-27 22:37:51 +0800

    ignore .env file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+require("dotenv").config();
+const PORT = process.env.PORT || 5000 ;
 app.use(cors());
 app.use(express.json());
 
@@ -10,6 +11,6 @@ app.get("/",(req,res)=>{
     res.send({message: "hello BIjay"})
 })
 
-app.listen(5000, () => {
-  console.log("Server Running");
+app.listen(PORT, () => {
+  console.log(`server is running on PORT ${PORT}`);
 });
